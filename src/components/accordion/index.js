ccf823bdafd83aca4bfc481e0fa0a66deeb1009f
shernk <31926953+shernk@users.noreply.github.com>
2020-10-18 16:38:20 +0700

    add footer

--- src/components/accordion/index.js
+++ src/components/accordion/index.js
@@ -41,7 +41,7 @@ Accordion.Header = function AccordionHeader({ children, ...restProps }) {
   const { toggleShow, setToggleShow } = useContext(ToggleContext);
 
   return (
-    <Header onClick={() => setToggleShow(!toggleShow)} {...restProps}>
+    <Header onClick={() => setToggleShow((toggleShow) => !toggleShow)} {...restProps}>
       {children}
       {toggleShow ? (
         <img src="/images/icons/close-slim.png" alt="Close" />
--- src/pages/homes/home.js
+++ src/pages/homes/home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import JumbotronContainer from "../../containers/jumbotron/jumbotron";
 import AccordionContainer from "../../containers/accordion/accordion";
+import FooterContainer from "../../containers/footer/footer";
 
 export default function Home() {
   return (
     <>
       <JumbotronContainer></JumbotronContainer>
       <AccordionContainer></AccordionContainer>
+      <FooterContainer></FooterContainer>
     </>
   );
 }
