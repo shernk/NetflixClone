223e28b2a3e84bb1cd96e103507f498423b19fe9
shernk <31926953+shernk@users.noreply.github.com>
2020-10-19 20:50:02 +0700

    set up route demo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
+import * as ROUTES from './routes/routes'
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from "./pages/homes/home";
+import SignIn from "./pages/signin/signin";
 
 export function App() {
   return (
-      <Home></Home>
+    <Router>
+      <Route exact path={ROUTES.SIGNIN}>
+        <SignIn />
+      </Route>
+      <Route exact path={ROUTES.HOME}>
+        <Home />
+      </Route>
+    </Router>
   );
 }
