02c236f43e1a09fc3b5ee9ae896980aef11f2adb
shernk <31926953+shernk@users.noreply.github.com>
2020-10-20 21:00:22 +0700

    finished home page

--- src/pages/homes/home.js
+++ src/pages/homes/home.js
@@ -1,13 +1,33 @@
 import React from "react";
-import {JumbotronContainer} from "../../containers/jumbotron/jumbotron";
+import { JumbotronContainer } from "../../containers/jumbotron/jumbotron";
 import { HeaderContainer } from "../../containers/header/header";
-import {AccordionContainer} from "../../containers/accordion/accordion";
-import {FooterContainer} from "../../containers/footer/footer";
+import { AccordionContainer } from "../../containers/accordion/accordion";
+import { FooterContainer } from "../../containers/footer/footer";
+import { Feature, OptForm } from "../../components";
 
 export default function Home() {
   return (
     <>
-      <HeaderContainer></HeaderContainer>
+      <HeaderContainer>
+        <Feature>
+          <Feature.Title>
+            Unlimited films, TV programmes and more.
+          </Feature.Title>
+          <Feature.SubTitle>
+            Watch anywhere. Cancel at any time.
+          </Feature.SubTitle>
+          <OptForm>
+            <OptForm.Input placeholder="Email address" />
+            <OptForm.Button>Try it now</OptForm.Button>
+            <OptForm.Break />
+            <OptForm.Text>
+              Ready to watch? Enter your email to create or restart your
+              membership.
+            </OptForm.Text>
+          </OptForm>
+        </Feature>
+      </HeaderContainer>
+
       <JumbotronContainer></JumbotronContainer>
       <AccordionContainer></AccordionContainer>
       <FooterContainer></FooterContainer>
