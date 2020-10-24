ec5947a57b496a75578c0700eda04611d2821513
shernk <31926953+shernk@users.noreply.github.com>
2020-10-24 15:42:09 +0700

    get database from firestore

--- src/pages/browse/browse.js
+++ src/pages/browse/browse.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import {useContent} from '../../hooks/index';
 
 export default function Browse(){
-  // const {series} = useContent('series');
-  // const {films} = useContent('films');
+  const {series} = useContent('series');
+  const {films} = useContent('films');
 
   return <p>browse page</p>;
 }
