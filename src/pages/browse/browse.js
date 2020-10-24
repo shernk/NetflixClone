13e804e7bc11cf3cd8b61d15d03c37e9b30df72d
shernk <31926953+shernk@users.noreply.github.com>
2020-10-24 16:23:57 +0700

    build up schema

--- src/pages/browse/browse.js
+++ src/pages/browse/browse.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import {useContent} from '../../hooks/index';
+import Schema from '../../schema/schema'; 
 
 export default function Browse(){
   const {series} = useContent('series');
   const {films} = useContent('films');
+  //? firestore database is out off
+  //TODO: have to create new data
+  const slides = Schema({series, films});
+  console.log(films);
 
   return <p>browse page</p>;
 }
