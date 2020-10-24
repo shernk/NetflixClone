3630f94b78b7e1ee2ff5ce59d9a170bb19c9b501
shernk <31926953+shernk@users.noreply.github.com>
2020-10-24 23:12:07 +0700

    set up new database

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -1,21 +1,20 @@
 import Firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
-import { seedDatabase } from "../seed";
+// import { seedDatabase } from "../seed";
 
 const config = {
-  apiKey: "AIzaSyA80d5dhXEZozWq9wieH4yR2ADe9BErwQI",
-  authDomain: "netflix-clone-2b71d.firebaseapp.com",
-  databaseURL: "https://netflix-clone-2b71d.firebaseio.com",
-  projectId: "netflix-clone-2b71d",
-  storageBucket: "netflix-clone-2b71d.appspot.com",
-  messagingSenderId: "1037861642454",
-  appId: "1:1037861642454:web:3566a5cf140569ed53a977",
-  measurementId: "G-W3Q417SP3Z",
+  apiKey: "AIzaSyB3tFeED17nNirIqemhQmOFXt6xi8w3R8U",
+  authDomain: "netflixclonedata.firebaseapp.com",
+  databaseURL: "https://netflixclonedata.firebaseio.com",
+  projectId: "netflixclonedata",
+  storageBucket: "netflixclonedata.appspot.com",
+  messagingSenderId: "781059873845",
+  appId: "1:781059873845:web:2331bb7d10157a9356257c",
 };
 
 const firebase = Firebase.initializeApp(config);
 
-seedDatabase(firebase);
+// seedDatabase(firebase);
 
 export { firebase };
