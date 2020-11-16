a9ae61c3791b73e451793c13388b88355c27106e
shernk <31926953+shernk@users.noreply.github.com>
2020-11-16 21:23:34 +0700

    clean code

--- src/routes/protected-route.js
+++ src/routes/protected-route.js
@@ -5,7 +5,6 @@ export function ProtectedRoute({user, children, ...rest}){
   return(
     <Route 
       {...rest}
-      //? why location is matter, let's find out
       render={({ location }) => {
         if(user){
           return children;
