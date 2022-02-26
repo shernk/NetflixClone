100b87b60597be32bb7a3dcf185a4c1b753f7353
Sherk <phanquocthinh.dng@gmail.com>
2022-02-26 20:20:13 +0700

    new config firebase

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -4,13 +4,12 @@ import "firebase/auth";
 import { seedDatabase } from "../seed";
 
 const config = {
-  apiKey: "AIzaSyAJxuYodttx9QFPMd8yj-dsmI6oIIcKTcM",
-  authDomain: "netflix-clone-770bf.firebaseapp.com",
-  projectId: "netflix-clone-770bf",
-  storageBucket: "netflix-clone-770bf.appspot.com",
-  messagingSenderId: "703907708095",
-  appId: "1:703907708095:web:3d9125dac36f64ed775a0a",
-  measurementId: "G-49QR08ST3F",
+  apiKey: "AIzaSyCADwvOdV3IGI33oT7EIZ188EEFWao93wY",
+  authDomain: "netflix-clone-26022022.firebaseapp.com",
+  projectId: "netflix-clone-26022022",
+  storageBucket: "netflix-clone-26022022.appspot.com",
+  messagingSenderId: "589965929285",
+  appId: "1:589965929285:web:3c6330064087b9ecd26c2c",
 };
 
 const firebase = Firebase.initializeApp(config);
